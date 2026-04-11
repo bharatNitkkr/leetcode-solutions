0c414520b439c086a002aa43d654a44726899901
bharatNitkkr <bharat157379@gmail.com>
2026-04-11 23:38:20 +0530

    Create 300. Longest increasing subsequence.java

---
+++ 300. Longest increasing subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,1);
+        int maxLen = 1;
+    
+        for(int i=1;i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i] = Math.max(dp[j]+1,dp[i]);
+                    maxLen = Math.max(maxLen,dp[i]);
+
+                }
+
+            }
+        }
+        return maxLen;
+    }
+}
